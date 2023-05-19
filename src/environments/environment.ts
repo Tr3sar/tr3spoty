37c048e60fcc5d0208f8ac19002add5ee2b3cac9
tr3sar <josepcdg@gmail.com>
2023-05-19 20:02:55 +0200

    variables de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,6 @@
 export const environment = {
     production: false,
+    spotifyClientId: 'TU_ID_DE_CLIENTE_DE_SPOTIFY',
+    spotifyRedirectUri: 'TU_URI_DE_REDIRECCIONAMIENTO',
+    spotifyScope: 'user-read-playback-state'
   };
