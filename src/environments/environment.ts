55b9f6b9a375c4805d317b43ff0b1eea496a3d17
tr3sar <josepcdg@gmail.com>
2023-05-19 20:06:03 +0200

    Prova gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
     production: false,
     spotifyClientId: 'TU_ID_DE_CLIENTE_DE_SPOTIFY',
     spotifyRedirectUri: 'TU_URI_DE_REDIRECCIONAMIENTO',
-    spotifyScope: 'user-read-playback-state'
+    spotifyScope: 'user-read-playback-state',
+    dsfsdf: 'asd'
   };
