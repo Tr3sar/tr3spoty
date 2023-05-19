e713980e04b0eb1b7e336ef756c7dd06762ad17e
tr3sar <josepcdg@gmail.com>
2023-05-19 18:27:01 +0200

    rutes aplicació

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayerComponent } from './player/player.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "inici", pathMatch: "full"},
+  {path: "inici",component: PlayerComponent},
+  {path: "busqueda", component: SearchComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
