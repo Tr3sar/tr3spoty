07469efdb5276060a2732ec4097a8c356396a233
tr3sar <josepcdg@gmail.com>
2023-05-19 19:17:44 +0200

    afegir angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerModule } from './player/player.module';
 import { SearchModule } from './search/search.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { SearchModule } from './search/search.module';
     AppRoutingModule,
 
     PlayerModule,
-    SearchModule
+    SearchModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
