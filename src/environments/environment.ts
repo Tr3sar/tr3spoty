8d457f4e780478f3e4adf3022052dfb7a9672541
tr3sar <josepcdg@gmail.com>
2023-05-19 20:12:29 +0200

    actualización environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
     production: false,
-    spotifyClientId: 'TU_ID_DE_CLIENTE_DE_SPOTIFY',
-    spotifyRedirectUri: 'TU_URI_DE_REDIRECCIONAMIENTO',
-    spotifyScope: 'user-read-playback-state',
-    dsfsdf: 'asd'
+    spotifyClientId: 'SECRET_SPOTIFY_CLIENT_ID',
+    spotifyClientSecret: 'SECRET_SPOTIFY_CLIENT_SECRET'
   };
